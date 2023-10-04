1d51028b8eae4e864d434cb1f6eb449a2fa756fc
pantcr7 <prasantpant141@gmail.com>
2023-10-04 13:41:09 +0545

    [fixed] i variable is defined in Support User Branch Settin.js'

--- ce_customization/ce_customization/doctype/support_user_branch_setting/support_user_branch_setting.js
+++ ce_customization/ce_customization/doctype/support_user_branch_setting/support_user_branch_setting.js
@@ -17,7 +17,7 @@ before_load: function (frm) {
                     //     frm.doc.support_usersite.splice(frm.doc.support_usersite[i], 1)
                     // }
                  
-                 for (i = 0; i < r.message.length; i++) {
+                 for (var i = 0; i < r.message.length; i++) {
                  
                     let mail=r.message[i].email;
                     let name=r.message[i].name;
@@ -51,12 +51,12 @@ before_load: function (frm) {
          })
          
      },
-     refresh: function(frm) {
-        //cur_frm.fields_dict['support_usersite'].grid.wrapper.find('.grid-remove-rows').hide();
-        frm.set_df_property('support_usersite', 'cannot_add_rows', true); // Hide add row button
-        frm.set_df_property('support_usersite', 'cannot_delete_rows', true); // Hide delete button
-        frm.set_df_property('support_usersite', 'cannot_delete_all_rows', true); // Hide delete all button
-     }
+    //  refresh: function(frm) {
+    //     //cur_frm.fields_dict['support_usersite'].grid.wrapper.find('.grid-remove-rows').hide();
+    //     frm.set_df_property('support_usersite', 'cannot_add_rows', true); // Hide add row button
+    //     frm.set_df_property('support_usersite', 'cannot_delete_rows', true); // Hide delete button
+    //     frm.set_df_property('support_usersite', 'cannot_delete_all_rows', true); // Hide delete all button
+    //  }
  });
  
 
