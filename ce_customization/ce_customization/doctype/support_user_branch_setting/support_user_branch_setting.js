8aad616076489b8c81945b96ff78db2eeb8478c4
pantcr7 <prasantpant141@gmail.com>
2023-10-04 13:44:59 +0545

    delete and add buttons removed from Support User Branch Setting doctype

--- ce_customization/ce_customization/doctype/support_user_branch_setting/support_user_branch_setting.js
+++ ce_customization/ce_customization/doctype/support_user_branch_setting/support_user_branch_setting.js
@@ -51,12 +51,12 @@ before_load: function (frm) {
          })
          
      },
-    //  refresh: function(frm) {
-    //     //cur_frm.fields_dict['support_usersite'].grid.wrapper.find('.grid-remove-rows').hide();
-    //     frm.set_df_property('support_usersite', 'cannot_add_rows', true); // Hide add row button
-    //     frm.set_df_property('support_usersite', 'cannot_delete_rows', true); // Hide delete button
-    //     frm.set_df_property('support_usersite', 'cannot_delete_all_rows', true); // Hide delete all button
-    //  }
+     refresh: function(frm) {
+        //cur_frm.fields_dict['support_usersite'].grid.wrapper.find('.grid-remove-rows').hide();
+        frm.set_df_property('support_usersite', 'cannot_add_rows', true); // Hide add row button
+        frm.set_df_property('support_usersite', 'cannot_delete_rows', true); // Hide delete button
+        frm.set_df_property('support_usersite', 'cannot_delete_all_rows', true); // Hide delete all button
+     }
  });
  
 
