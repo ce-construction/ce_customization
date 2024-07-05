44ea6b8922a04f58114d930ca1f23a0747d4b155
anjanadhakal <anjanadhakal09@gmail.com>
2024-07-05 11:48:56 +0545

    Modified

---
+++ ce_customization/ce_customization/doctype/project_drawing/test_project_drawing.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ce construction and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProjectDrawing(FrappeTestCase):
+	pass
